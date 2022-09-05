8bdaa9bc126581e5a8e826167cb5469d39964943
nihaojob <nihaojob@users.noreply.github.com>
2022-09-05 05:27:01 +0000

    deploy: 55cb6ede20b4fd47fb7cb5d01cfdf61dec8797e3

---
+++ js/about.ee748372.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksvg"]=self["webpackChunksvg"]||[]).push([[443],{4613:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],a=n(3736),l={},r=(0,a.Z)(l,s,u,!1,null,null,null),c=r.exports}}]);
+//# sourceMappingURL=about.ee748372.js.map
