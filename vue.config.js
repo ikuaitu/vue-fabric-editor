ac9f99f64d32a17a63f9bcd619f88f846dd7de22
秦少卫 <nihaojob@163.com>
2022-09-05 13:37:23 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-fabric-editor/'
+    : '/',
   transpileDependencies: true,
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
