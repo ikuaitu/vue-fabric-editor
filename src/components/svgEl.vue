c91efbe45120f99feea097215c1441acff280b69
houlonglong <31363537@qq.com>
2023-02-23 21:52:47 +0800

    fix(svg拖动事件): 修复画布拖拽
    * fix(是否在画布区内): 添加元素拖动事件
    * fix(是否在画布区内): 添加元素拖动事件
    * fix(svg拖动事件): 修复是否在画布内

--- src/components/svgEl.vue
+++ src/components/svgEl.vue
@@ -8,85 +8,236 @@
 
 <template>
   <div>
-    <Divider plain orientation="left">{{ $t('cartoon')  }}</Divider>
+    <Divider plain orientation="left">{{ $t('cartoon') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(460, 489)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(460, 489)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
     <Divider plain orientation="left">{{ $t('fruits') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(386, 409)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(386, 409)"
+        @click="addItem"
+        ::draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
     <Divider plain orientation="left">{{ $t('sports') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(410, 459)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(410, 459)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
     <Divider plain orientation="left">{{ $t('seasons') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(40, 49)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(40, 49)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
     <Divider plain orientation="left">{{ $t('eletronics') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(50, 75)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(50, 75)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
     <Divider plain orientation="left">{{ $t('fruits') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(76, 89)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(76, 89)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
-    <Divider plain orientation="left">{{ $t('clothes')  }}</Divider>
+    <Divider plain orientation="left">{{ $t('clothes') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(89, 136)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(89, 136)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
-    <Divider plain orientation="left">{{ $t('flags')  }}</Divider>
+    <Divider plain orientation="left">{{ $t('flags') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(137, 151)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(137, 151)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
-    <Divider plain orientation="left">{{ $t('threes')  }}</Divider>
+    <Divider plain orientation="left">{{ $t('threes') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(152, 181)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(152, 181)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
-    <Divider plain orientation="left">{{ $t('food')  }}</Divider>
+    <Divider plain orientation="left">{{ $t('food') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(182, 201)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(182, 201)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
-    <Divider plain orientation="left">{{ $t('clothes')  }}</Divider>
+    <Divider plain orientation="left">{{ $t('clothes') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(202, 222)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(202, 222)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
     <Divider plain orientation="left">{{ $t('medals') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(223, 252)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(223, 252)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
     <Divider plain orientation="left">{{ $t('business') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(253, 261)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(253, 261)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
     <Divider plain orientation="left">{{ $t('activity') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(262, 270)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(262, 270)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
     <Divider plain orientation="left">{{ $t('fruits') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(271, 300)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(271, 300)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
     <Divider plain orientation="left">{{ $t('vintage') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(301, 350)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(301, 350)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
-    <Divider plain orientation="left">{{  $t('cartoon') }}</Divider>
+    <Divider plain orientation="left">{{ $t('cartoon') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(351, 385)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(351, 385)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
 
     <Divider plain orientation="left">{{ $t('animals') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(490, 519)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(490, 519)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
 
     <Divider plain orientation="left">{{ $t('hand_painted') }}</Divider>
     <div class="box">
-        <img v-lazy="`./svg/${item}.svg`" alt="" :key="item" v-for="(item) in getIndex(0, 39)" @click="addItem" >
+      <img
+        v-lazy="`./svg/${item}.svg`"
+        alt=""
+        :key="item"
+        v-for="item in getIndex(0, 39)"
+        @click="addItem"
+        :draggable="true"
+        @dragend="dragItem(item, $event)"
+      />
     </div>
-
   </div>
 </template>
 
@@ -94,7 +245,10 @@
 import { v4 as uuid } from 'uuid';
 
 const defaultPosition = {
-  left: 100, top: 100, shadow: '', fontFamily: '1-1',
+  left: 100,
+  top: 100,
+  shadow: '',
+  fontFamily: '1-1',
 };
 export default {
   name: 'ToolBar',
@@ -104,12 +258,36 @@ export default {
       arr: [],
     };
   },
-  created() {
-  },
+  created() {},
   methods: {
     getIndex(start, end) {
       const arr = Array(end - (start - 1)).fill('');
-      return arr.map((item, i) => (i + start));
+      return arr.map((item, i) => i + start);
+    },
+    /* eslint-disable */
+    dragItem(item, event) {
+      const url = event.target.src;
+      const { left, top, right, bottom } = this.canvas.c
+        .getSelectionElement()
+        .getBoundingClientRect();
+      if (event.x < left || event.y < top || event.x > right || event.x > bottom) return;
+      const point = {
+        x: event.x - left,
+        y: event.y - top,
+      };
+      const pointerVpt = this.canvas.c.restorePointerVpt(point);
+      this.fabric.loadSVGFromURL(url, (objects, options) => {
+        const item = this.fabric.util.groupSVGElements(objects, {
+          shadow: '',
+          fontFamily: 'arial',
+          id: uuid(),
+          name: 'svg元素',
+        });
+        item.left = pointerVpt.x - item.width / 2;
+        item.top = pointerVpt.y;
+        this.canvas.c.add(item);
+        this.canvas.c.requestRenderAll();
+      });
     },
     // 按照类型渲染
     addItem(e) {
@@ -122,7 +300,7 @@ export default {
           name: 'svg元素',
         });
         this.canvas.c.add(item);
-        this.canvas.c.renderAll();
+        this.canvas.c.requestRenderAll();
       });
     },
   },
@@ -130,13 +308,12 @@ export default {
 </script>
 
 <style scoped lang="less">
-
 img {
   display: inline-block;
   width: 53px;
   margin-left: 2px;
   margin-bottom: 2px;
-  background: #F5F5F5;
+  background: #f5f5f5;
   padding: 6px;
   cursor: pointer;
 }
