f416bc3ec081ad3e35e77c2fcbdb083d8b02bd64
nihaojob <nihaojob@users.noreply.github.com>
2023-02-23 13:46:22 +0000

    deploy: f22cf457ddbc1cd38001ed31aa7c0c1658e130be

---
+++ js/about.d0faa19a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksvg"]=self["webpackChunksvg"]||[]).push([[443],{1168:function(n,s,t){t.r(s),t.d(s,{default:function(){return i}});var u=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,s=n._self._c;return s("div",{staticClass:"about"},[s("h1",[n._v("This is an about page")])])}],a=t(1001),l={},r=(0,a.Z)(l,u,e,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.d0faa19a.js.map
