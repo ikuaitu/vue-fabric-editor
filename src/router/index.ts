39fc6f13a23cd200b6d8cff06c6d9ce91312f2ef
秦少卫 <nihaojob@163.com>
2023-03-28 11:52:35 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import routes from './routes';
 
 export default createRouter({
   routes,
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   scrollBehavior() {
     return { top: 0 };
   },
