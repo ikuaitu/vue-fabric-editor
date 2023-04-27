f468301f7126c7584aed5f7e658961ff41abe899
June <1601745371@qq.com>
2023-04-27 22:55:35 +0800

    feat: select hooks instead mixins and save components setup (#193)
    * feat: select hooks instead mixins and save components setup
    
    * fix: save and history compoment vue3

--- src/components/history.vue
+++ src/components/history.vue
@@ -1,103 +1,92 @@
 <!--
  * @Author: 秦少卫
  * @Date: 2022-09-03 19:16:55
- * @LastEditors: 秦少卫
- * @LastEditTime: 2023-02-26 19:58:01
+ * @LastEditors: June
+ * @LastEditTime: 2023-04-25 11:24:07
  * @Description: 回退重做
 -->
 
 <template>
   <div style="display: inline-block">
     <!-- 后退 -->
-    <Tooltip :content="$t('history.revocation') + `(${list.length})`">
-      <Button @click="undo" type="text" size="small" :disabled="!list.length">
+    <Tooltip :content="$t('history.revocation') + `(${state.list.length})`">
+      <Button @click="undo" type="text" size="small" :disabled="!state.list.length">
         <Icon type="ios-undo" size="20" />
       </Button>
     </Tooltip>
 
     <!-- 重做 -->
-    <Tooltip :content="$t('history.redo') + `(${redoList.length})`">
-      <Button @click="redo" type="text" size="small" :disabled="!redoList.length">
+    <Tooltip :content="$t('history.redo') + `(${state.redoList.length})`">
+      <Button @click="redo" type="text" size="small" :disabled="!state.redoList.length">
         <Icon type="ios-redo" size="20" />
       </Button>
     </Tooltip>
 
     <span class="time">
-      {{ time }}
+      {{ state.time }}
     </span>
   </div>
 </template>
 
-<script>
-import select from '@/mixins/select';
+<script setup name="ToolBar">
+import { reactive, onMounted } from 'vue';
 import { keyNames, hotkeys } from '@/core/initHotKeys';
-
+import useSelect from '@/hooks/select';
+const { $this, $t } = useSelect();
 const maxStep = 10;
-
-export default {
-  name: 'ToolBar',
-  mixins: [select],
-  data() {
-    return {
-      index: 0,
-      redoList: [],
-      list: [],
-      time: '',
-    };
-  },
-  created() {
-    // 有更新时记录进度
-    this.canvas.c.on({
-      'object:modified': this.save,
-      'selection:updated': this.save,
-    });
-
-    hotkeys(keyNames.ctrlz, this.undo);
-  },
-
-  methods: {
-    // 保存记录
-    save(event) {
-      // 过滤选择元素事件
-      const isSelect = event.action === undefined && event.e;
-      if (isSelect) return;
-      const data = this.canvas.editor.getJson();
-      if (this.list.length > maxStep) {
-        this.list.shift();
-      }
-      this.list.push(data);
-      this.getTime();
-    },
-    getTime() {
-      const myDate = new Date();
-      const str = myDate.toTimeString();
-      const timeStr = str.substring(0, 8);
-      this.time = timeStr;
-    },
-    // 后退
-    undo() {
-      if (this.list.length) {
-        const item = this.list.pop();
-        this.redoList.push(item);
-        this.renderCanvas(item);
-      }
-    },
-    // 重做
-    redo() {
-      if (this.redoList.length) {
-        const item = this.redoList.pop();
-        this.list.push(item);
-        this.renderCanvas(item);
-      }
-    },
-    // 根据数据渲染
-    renderCanvas(data) {
-      this.canvas.c.clear();
-      this.canvas.c.loadFromJSON(data, this.canvas.c.renderAll.bind(this.canvas.c));
-      this.canvas.c.requestRenderAll();
-    },
-  },
+const state = reactive({
+  index: 0,
+  redoList: [],
+  list: [],
+  time: '',
+});
+const getTime = () => {
+  const myDate = new Date();
+  const str = myDate.toTimeString();
+  const timeStr = str.substring(0, 8);
+  state.time = timeStr;
 };
+const save = (event) => {
+  // 过滤选择元素事件
+  const isSelect = event.action === undefined && event.e;
+  if (isSelect) return;
+  const data = $this.canvas.editor.getJson();
+  if (state.list.length > maxStep) {
+    state.list.shift();
+  }
+  state.list.push(data);
+  getTime();
+};
+// 根据数据渲染
+const renderCanvas = (data) => {
+  $this.canvas.c.clear();
+  $this.canvas.c.loadFromJSON(data, $this.canvas.c.renderAll.bind($this.canvas.c));
+  $this.canvas.c.requestRenderAll();
+};
+// 后退
+const undo = () => {
+  if (state.list.length) {
+    const item = state.list.pop();
+    state.redoList.push(item);
+    renderCanvas(item);
+  }
+};
+// 重做
+const redo = () => {
+  if (state.redoList.length) {
+    const item = state.redoList.pop();
+    state.list.push(item);
+    renderCanvas(item);
+  }
+};
+onMounted(() => {
+  // 有更新时记录进度
+  $this.canvas.c.on({
+    'object:modified': save,
+    'selection:updated': save,
+  });
+  hotkeys(keyNames.ctrlz, undo);
+});
 </script>
 
 <style scoped lang="less">
---
+++ typings/vue3-clipboard.d.ts
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: June
+ * @Date: 2023-04-23 21:43:24
+ * @LastEditors: June
+ * @LastEditTime: 2023-04-23 21:43:51
+ */
+declare module 'vue3-clipboard';
