19438e439b668ae40d245ea32a03827af7671133
秦少卫 <nihaojob@163.com>
2023-05-07 13:23:34 +0800

    fix(vue3): 删除多余依赖

--- typings/modules.ts
+++ typings/modules.ts
@@ -2,6 +2,5 @@
  * @Description: Declarations for node modules.
  */
 
-declare module 'vue3-clipboard';
 declare module 'view-ui-plus/dist/locale/zh-CN';
 declare module 'view-ui-plus/dist/locale/en-US';
--- typings/vue3-clipboard.d.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Descripttion:
- * @version:
- * @Author: June
- * @Date: 2023-04-23 21:43:24
- * @LastEditors: June
- * @LastEditTime: 2023-04-23 21:43:51
- */
-declare module 'vue3-clipboard';
