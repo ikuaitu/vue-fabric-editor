065dc9702617dff6f3c81b3095d1f48201178b87
June <1601745371@qq.com>
2023-05-08 22:36:43 +0800

    fix: 图层面板增加为空提示 (#202)
    * fix: 图层面板增加为空提示
    
    * fix: 优化hook event监听

--- src/hooks/select.js
+++ src/hooks/select.js
@@ -3,11 +3,11 @@
  * @version:
  * @Author: June
  * @Date: 2023-04-23 21:10:05
- * @LastEditors: 秦少卫
- * @LastEditTime: 2023-05-07 09:35:08
+ * @LastEditors: June
+ * @LastEditTime: 2023-05-08 18:20:21
  */
 
-import { inject, reactive, onMounted } from 'vue';
+import { inject, reactive, onMounted, onBeforeMount } from 'vue';
 import { SelectEvent, SelectMode } from '@/utils/event/types';
 // todo
 // interface Data {
@@ -31,30 +31,41 @@ export default function useSelect() {
   const canvas = inject('canvas');
   const event = inject('event');
 
+  const selectOne = (e) => {
+    state.mSelectMode = SelectMode.ONE;
+    state.mSelectId = e[0].id;
+    state.mSelectOneType = e[0].type;
+    state.mSelectIds = e.map((item) => item.id);
+  };
+
+  const selectMulti = (e) => {
+    state.mSelectMode = SelectMode.MULTI;
+    state.mSelectId = '';
+    state.mSelectIds = e.map((item) => item.id);
+  };
+
+  const selectCancel = () => {
+    state.mSelectId = '';
+    state.mSelectIds = [];
+    state.mSelectMode = '';
+    state.mSelectOneType = '';
+  };
+
   onMounted(() => {
-    event.on(SelectEvent.ONE, (e) => {
-      state.mSelectMode = SelectMode.ONE;
-      state.mSelectId = e[0].id;
-      state.mSelectOneType = e[0].type;
-      state.mSelectIds = e.map((item) => item.id);
-    });
+    event.on(SelectEvent.ONE, selectOne);
+    event.on(SelectEvent.MULTI, selectMulti);
+    event.on(SelectEvent.CANCEL, selectCancel);
+  });
 
-    event.on(SelectEvent.MULTI, (e) => {
-      state.mSelectMode = SelectMode.MULTI;
-      state.mSelectId = '';
-      state.mSelectIds = e.map((item) => item.id);
-    });
-
-    event.on(SelectEvent.CANCEL, () => {
-      state.mSelectId = '';
-      state.mSelectIds = [];
-      state.mSelectMode = '';
-      state.mSelectOneType = '';
-    });
+  onBeforeMount(() => {
+    event.off(SelectEvent.ONE, selectOne);
+    event.off(SelectEvent.MULTI, selectMulti);
+    event.off(SelectEvent.CANCEL, selectCancel);
   });
 
   return {
     fabric,
     canvas,
+    mixinState: state,
   };
 }
