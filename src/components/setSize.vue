74174e7ce808caac3b29d44f3b641d7c9b757ddc
bigFace2019 <55651401+bigFace2019@users.noreply.github.com>
2023-05-13 16:38:23 +0800

    Big face2019 refactor set size (#207)
    * refactor(国际化): i18n国际化提取为hook
    
    * refactor(setSize组件): setSize组件修改为set形式

--- src/components/setSize.vue
+++ src/components/setSize.vue
@@ -1,13 +1,13 @@
 <!--
  * @Author: 秦少卫
  * @Date: 2022-09-03 19:16:55
- * @LastEditors: June
- * @LastEditTime: 2023-04-02 23:51:49
- * @Description: 尺寸设置
+ * @LastEditors: bigFace2019 599069310@qq.com
+ * @LastEditTime: 2023-05-10 21:52:37
+ * @Description: 尺寸设置f
 -->
 
 <template>
-  <div v-if="!mSelectMode">
+  <div v-if="!mixinState.mSelectMode">
     <Divider plain orientation="left">{{ $t('size') }}</Divider>
     <Form :label-width="40">
       <FormItem :label="$t('width')" prop="name">
@@ -32,53 +32,50 @@
   </div>
 </template>
 
-<script>
-import select from '@/mixins/select';
+<script setup name="CanvasSize">
+import useSelect from '@/hooks/select';
+import useI18n from '@/hooks/useI18n';
+import { ref, onMounted, reactive } from 'vue';
+
 import EditorWorkspace from '@/core/EditorWorkspace';
 
-export default {
-  name: 'canvasSize',
-  mixins: [select],
-  inject: ['canvas', 'fabric'],
-  data() {
-    return {
-      width: 900,
-      height: 1200,
-      presetSize: [
-        {
-          label: this.$t('red_book_vertical'),
-          width: 900,
-          height: 1200,
-        },
-        {
-          label: this.$t('red_book_horizontal'),
-          width: 1200,
-          height: 900,
-        },
-        {
-          label: this.$t('phone_wallpaper'),
-          width: 1080,
-          height: 1920,
-        },
-      ],
-    };
+const { canvas, mixinState } = useSelect();
+const $t = useI18n();
+
+let width = ref(900);
+let height = ref(1200);
+let presetSize = reactive([
+  {
+    label: $t('red_book_vertical'),
+    width: 900,
+    height: 1200,
   },
-  mounted() {
-    this.canvas.editor.editorWorkspace = new EditorWorkspace(this.canvas.c, {
-      width: this.width,
-      height: this.height,
-    });
+  {
+    label: $t('red_book_horizontal'),
+    width: 1200,
+    height: 900,
   },
-  methods: {
-    setSizeBy(width, height) {
-      this.width = width;
-      this.height = height;
-      this.setSize();
-    },
-    setSize() {
-      this.canvas.editor.editorWorkspace.setSize(this.width, this.height);
-    },
+  {
+    label: $t('phone_wallpaper'),
+    width: 1080,
+    height: 1920,
   },
+]);
+
+onMounted(() => {
+  canvas.editor.editorWorkspace = new EditorWorkspace(canvas.c, {
+    width: width.value,
+    height: height.value,
+  });
+});
+
+const setSizeBy = (w, h) => {
+  width.value = w;
+  height.value = h;
+  setSize();
+};
+const setSize = () => {
+  canvas.editor.editorWorkspace.setSize(width.value, height.value);
 };
 </script>
 
