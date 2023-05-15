725190e783a09c769fba36ad9bda3714051d5326
bigFace2019 <55651401+bigFace2019@users.noreply.github.com>
2023-05-16 06:32:00 +0800

    refactor: lang,group,flip三个组件修改为组合式setup写法。 (#212)
    * refactor(国际化): i18n国际化提取为hook
    
    * refactor(setSize组件): setSize组件修改为set形式
    
    * refactor(lang.vue): 修改lang.vue为setup形式。
    
    * feat(group.vue): 修改为option组合式形式组件
    
    * feat(flip.vue): 翻转组件使用组合式setup方式。

--- src/components/group.vue
+++ src/components/group.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: 秦少卫
  * @Date: 2022-09-03 19:16:55
- * @LastEditors: June
- * @LastEditTime: 2023-04-02 23:50:05
+ * @LastEditors: bigFace2019 599069310@qq.com
+ * @LastEditTime: 2023-05-14 18:28:23
  * @Description: 组合与拆分组合
 -->
 
@@ -47,39 +47,27 @@
       </svg>
       拆分组合
     </Button>
-    <!-- </ButtonGroup> -->
   </div>
 </template>
 
-<script>
-import select from '@/mixins/select';
-// import { v4 as uuid } from 'uuid';
+<script setup name="ToolBar">
+import useSelect from '@/hooks/select';
+import { computed } from 'vue';
 
-export default {
-  name: 'ToolBar',
-  mixins: [select],
-  data() {
-    return {};
-  },
-  computed: {
-    // 单选且等于组元素
-    isGroup() {
-      return this.mSelectMode === 'one' && this.mSelectOneType === 'group';
-    },
-    // 是否为多选
-    isMultiple() {
-      return this.mSelectMode === 'multiple';
-    },
-  },
-  methods: {
-    // 拆分组
-    unGroup() {
-      this.canvas.editor.unGroup();
-    },
-    group() {
-      this.canvas.editor.group();
-    },
-  },
+const { canvas, mixinState } = useSelect();
+
+// 单选且等于组元素
+let isGroup = computed(
+  () => mixinState.mSelectMode === 'one' && mixinState.mSelectOneType === 'group'
+);
+// 是否为多选
+let isMultiple = computed(() => mixinState.mSelectMode === 'multiple');
+// 拆分组
+const unGroup = () => {
+  canvas.editor.unGroup();
+};
+const group = () => {
+  canvas.editor.group();
 };
 </script>
 <style scoped lang="less">
