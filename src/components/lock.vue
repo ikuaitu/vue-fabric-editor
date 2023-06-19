c274063b151c243ed5b9e2729cb941d2d77e3b20
June <1601745371@qq.com>
2023-06-20 02:38:26 +0800

    fix: lock component setup

--- src/components/lock.vue
+++ src/components/lock.vue
@@ -7,15 +7,18 @@
 -->
 
 <template>
-  <Tooltip :content="$t('quick.lock')" v-if="mSelectMode === 'one'">
+  <Tooltip :content="$t('quick.lock')" v-if="mixinState.mSelectMode === 'one'">
     <Button v-if="isLock" @click="doLock(false)" icon="md-lock" type="text"></Button>
     <Button v-else @click="doLock(true)" icon="md-unlock" type="text"></Button>
   </Tooltip>
 </template>
 
-<script>
-import select from '@/mixins/select';
+<script setup name="Lock">
+import useSelect from '@/hooks/select';
+import { onBeforeUnmount, onMounted } from 'vue';
 
+const event = inject('event');
+const { canvas, mixinState } = useSelect();
 const lockAttrs = [
   'lockMovementX',
   'lockMovementY',
@@ -23,52 +26,50 @@ const lockAttrs = [
   'lockScalingX',
   'lockScalingY',
 ];
-export default {
-  name: 'ToolBar',
-  mixins: [select],
-  data() {
-    return {
-      isLock: false,
-    };
-  },
-  created() {
-    this.event.on('selectOne', (items) => {
-      this.isLock = !items[0].hasControls;
-      // eslint-disable-next-line prefer-destructuring
-      this.mSelectActive = items[0];
-    });
-  },
-  methods: {
-    doLock(isLock) {
-      isLock ? this.lock() : this.unLock();
-    },
-    lock() {
-      // 修改自定义属性
-      this.mSelectActive.hasControls = false;
-      // 修改默认属性
-      lockAttrs.forEach((key) => {
-        this.mSelectActive[key] = true;
-      });
+const isLock = ref(false);
+const lock = () => {
+  // 修改自定义属性
+  mixinState.mSelectActive.hasControls = false;
+  // 修改默认属性
+  lockAttrs.forEach((key) => {
+    mixinState.mSelectActive[key] = true;
+  });
 
-      this.mSelectActive.selectable = false;
+  mixinState.mSelectActive.selectable = false;
 
-      this.isLock = true;
-      this.canvas.c.renderAll();
-    },
-    unLock() {
-      // 修改自定义属性
-      this.mSelectActive.hasControls = true;
-      // 修改默认属性
-      lockAttrs.forEach((key) => {
-        this.mSelectActive[key] = false;
-      });
-      this.mSelectActive.selectable = true;
-
-      this.isLock = false;
-      this.canvas.c.renderAll();
-    },
-  },
+  isLock.value = true;
+  canvas.c.renderAll();
 };
+const unLock = () => {
+  // 修改自定义属性
+  mixinState.mSelectActive.hasControls = true;
+  // 修改默认属性
+  lockAttrs.forEach((key) => {
+    mixinState.mSelectActive[key] = false;
+  });
+  mixinState.mSelectActive.selectable = true;
+
+  isLock.value = false;
+  canvas.c.renderAll();
+};
+
+const doLock = (isLock) => {
+  isLock ? lock() : unLock();
+};
+
+const handleSelected = (items) => {
+  isLock.value = !items[0].hasControls;
+  // eslint-disable-next-line prefer-destructuring
+  mixinState.mSelectActive = items[0];
+};
+
+onMounted(() => {
+  event.on('selectOne', handleSelected);
+});
+
+onBeforeUnmount(() => {
+  event.off('selectOne', handleSelected);
+});
 </script>
 
 <style scoped lang="less">
