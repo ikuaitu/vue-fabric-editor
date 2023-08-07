666ae699e226d67e0f03ccf9e8bde4ed3df0752e
bamzc <baizhaoce@01zhuanche.com>
2023-08-07 17:37:43 +0800

    组内元素优化-双击处理

--- src/core/plugin/GroupPlugin.ts
+++ src/core/plugin/GroupPlugin.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: 秦少卫
  * @Date: 2023-06-20 13:21:10
- * @LastEditors: 秦少卫
- * @LastEditTime: 2023-06-20 13:42:32
+ * @LastEditors: bamzc
+ * @LastEditTime: 2023-08-07 17:08:21
  * @Description: 组合拆分组合插件
  */
 
@@ -25,29 +25,106 @@ class GroupPlugin {
   unGroup() {
     const activeObject = this.canvas.getActiveObject() as fabric.Group;
     if (!activeObject) return;
+    const canvas = this.canvas;
+    // 获取拆分组之前的所有对象
+    const allObjects = canvas.getObjects();
+
+    const activeObjects = activeObject.getObjects();
     // 先获取当前选中的对象，然后打散
     activeObject.toActiveSelection();
-    activeObject.getObjects().forEach((item: fabric.Object) => {
-      item.set('id', uuid());
+
+    const newAllObjectsNotActive: fabric.Object[] = [];
+
+    // 遍历打散重新组合的所有对象，并且过滤掉组合内的对象
+    canvas.getObjects().map((element: fabric.Object) => {
+      if (activeObjects.some((item) => item.id === element.id)) {
+        return;
+      }
+      newAllObjectsNotActive.push(element);
     });
-    this.canvas.discardActiveObject().renderAll();
+    let activeObjectIndex = 0;
+
+    // 找到原来所有对象中，组合对象的位置
+    allObjects.some((item, i) => {
+      if (item.id === activeObject.id) {
+        activeObjectIndex = i;
+      }
+    });
+
+    if (activeObjectIndex === 0) {
+      newAllObjectsNotActive.concat(activeObjects);
+    } else {
+      // 从原位置进行组合拆分，避免自动将拆分的组合置顶处理
+      // Array.prototype.splice.apply(
+      //   newAllObjectsNotActive,
+      //   [activeObjectIndex, 0].concat(activeObjects)
+      // );
+      newAllObjectsNotActive.splice(activeObjectIndex, 0, ...activeObjects);
+    }
+
+    // 重新将所有的元素对象进行排序
+    canvas._objects = newAllObjectsNotActive;
+    canvas.discardActiveObject().renderAll();
   }
 
   group() {
     // 组合元素
     const activeObj = this.canvas.getActiveObject() as fabric.ActiveSelection;
     if (!activeObj) return;
+    const canvas = this.canvas;
+    // 获取组合之前的所有对象
+    const allObjects = canvas.getObjects();
     const activegroup = activeObj.toGroup();
+
     const objectsInGroup = activegroup.getObjects();
-    activegroup.clone((newgroup: fabric.Group) => {
-      newgroup.set('id', uuid());
-      this.canvas.remove(activegroup);
-      objectsInGroup.forEach((object) => {
-        this.canvas.remove(object);
-      });
-      this.canvas.add(newgroup);
-      this.canvas.setActiveObject(newgroup);
+
+    let needIndex = -1;
+
+    // 找到原来所有对象中，组合里面对象位置最高的那一个
+    allObjects.some((item, i) => {
+      if (objectsInGroup.some((obj) => obj.id === item.id)) {
+        needIndex = i;
+      }
     });
+
+    // 找到原来所有对象中，组合里面对象的所有位置
+    const oldGroupsPos: fabric.Object[] = [];
+    allObjects.map((item) => {
+      if (objectsInGroup.some((obj) => obj.id === item.id)) {
+        oldGroupsPos.push(item);
+      }
+    });
+
+    console.log('元素排序前', allObjects, needIndex);
+
+    activegroup.set('id', uuid());
+    activegroup._objects = oldGroupsPos;
+    objectsInGroup.forEach((object) => {
+      this.canvas.remove(object);
+    });
+    this.canvas.remove(activegroup);
+    canvas.add(activegroup);
+    canvas.setActiveObject(activegroup);
+
+    const newAllObjectsNotActive: fabric.Object[] = [];
+
+    canvas.getObjects().map((element: fabric.Object) => {
+      if (activegroup.id === element.id) {
+        return;
+      }
+      newAllObjectsNotActive.push(element);
+    });
+    needIndex = needIndex === -1 ? 0 : needIndex - (objectsInGroup.length - 1);
+    // 从未组合之前，需要组合的元素，并且其中位置最高的元素，从这个位置进行组合插入
+    if (needIndex === 0) {
+      newAllObjectsNotActive.push(activegroup);
+    } else {
+      newAllObjectsNotActive.splice(needIndex, 0, activegroup);
+    }
+    console.log('元素排序后', newAllObjectsNotActive, needIndex);
+    // 重新将所有的元素对象进行排序
+    canvas._objects = newAllObjectsNotActive;
+    canvas.renderAll();
   }
 
   contextMenu() {
