e323bfa967d4dcf83cb6a38256d1400aa79d8c3c
xing.huang <xing.huang@sanofi.com>
2023-09-06 10:51:41 +0800

    feat: 1. 修复部分浏览器滚动条css无效

--- src/App.vue
+++ src/App.vue
@@ -15,16 +15,4 @@ svg {
 .vue-waterfall-easy-scroll {
   overflow: inherit !important;
 }
-*::-webkit-scrollbar {
-  width: 6px;
-  height: 6px;
-}
-*::-webkit-scrollbar-thumb {
-  border-radius: 8px;
-  background: rgba(0, 0, 0, 0.2);
-}
-*::-webkit-scrollbar-track {
-  border-radius: 0;
-  background: rgba(0, 0, 0, 0.1);
-}
 </style>
