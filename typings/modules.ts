864ce27d97b6040633c3fa996057cb81edf878e3
June <1601745371@qq.com>
2024-03-17 22:04:06 +0800

    fix: 处理fontfaceobserver类型警告问题

--- typings/modules.ts
+++ typings/modules.ts
@@ -1,6 +1,14 @@
 /*
+ * @Author: June
+ * @Description:
+ * @Date: 2024-01-06 21:47:34
+ * @LastEditors: June
+ * @LastEditTime: 2024-03-17 22:03:28
+ */
+/*
  * @Description: Declarations for node modules.
  */
 
 declare module 'view-ui-plus/dist/locale/zh-CN';
 declare module 'view-ui-plus/dist/locale/en-US';
+declare module 'fontfaceobserver';
