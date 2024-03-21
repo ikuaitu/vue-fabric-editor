4844c9430f3ad971c5c61c4da77d062c19645791
June <1601745371@qq.com>
2024-03-21 14:54:43 +0800

    fix: 处理ts fontfaceobserver包类型问题

--- typings/modules.ts
+++ typings/modules.ts
@@ -2,8 +2,8 @@
  * @Author: June
  * @Description:
  * @Date: 2024-01-06 21:47:34
- * @LastEditors: June
- * @LastEditTime: 2024-03-17 22:03:28
+ * @LastEditors: June 1601745371@qq.com
+ * @LastEditTime: 2024-03-21 14:48:22
  */
 /*
  * @Description: Declarations for node modules.
@@ -11,4 +11,3 @@
 
 declare module 'view-ui-plus/dist/locale/zh-CN';
 declare module 'view-ui-plus/dist/locale/en-US';
-declare module 'fontfaceobserver';
