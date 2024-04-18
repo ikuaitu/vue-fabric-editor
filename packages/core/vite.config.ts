41324818189919db2642492f447d3573e30e7858
June <1601745371@qq.com>
2024-04-18 11:37:34 +0800

    chore: 优化sdk打包命令 (#339)

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -3,8 +3,8 @@
  * @version:
  * @Author: June
  * @Date: 2023-04-24 00:25:39
- * @LastEditors: 秦少卫
- * @LastEditTime: 2024-03-17 13:46:56
+ * @LastEditors: June 1601745371@qq.com
+ * @LastEditTime: 2024-04-18 10:14:25
  */
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint'; //导入包
@@ -12,12 +12,14 @@ import { resolve } from 'path';
 
 const config = () => {
   return {
+    base: './',
     build: {
       lib: {
-        entry: resolve(__dirname, './packages/core/index.ts'),
+        entry: resolve(__dirname, './index.ts'),
         name: 'Kuaitu',
         fileName: 'index',
       },
+      outDir: resolve(__dirname, '../../dist'),
     },
     plugins: [
       // 增加下面的配置项,这样在运行时就能检查eslint规范
