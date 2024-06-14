e9c6d1c1f4badbb5937fe0753c9c805a4c67ed2f
June <1601745371@qq.com>
2024-06-14 17:09:50 +0800

    chore(App): router加载完成再mount

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import App from './App';
+import App from './App.vue';
 import router from './router';
 import ViewUiPlus from 'view-ui-plus';
 import 'view-ui-plus/dist/styles/viewuiplus.css';
@@ -12,8 +12,14 @@ import { VueMasonryPlugin } from 'vue-masonry';
 
 import i18n from './language/index';
 
-const app = createApp(App);
-
-app.use(VueMasonryPlugin);
-
-app.use(router).use(i18n).use(VueLazyLoad, {}).use(ViewUiPlus).mount('#app');
+async function bootstrap() {
+  const app = createApp(App);
+  app.use(VueMasonryPlugin);
+  app.use(router);
+  app.use(i18n);
+  app.use(VueLazyLoad, {});
+  app.use(ViewUiPlus);
+  await router.isReady();
+  app.mount('#app');
+}
+bootstrap();
