2773d0c451885d3273165a039f3b7ea4760c74de
Alice Peng <1399789151@qq.com>
2024-07-09 12:01:36 +0800

    fix(component):  新增空瀑布流组件

---
+++ src/components/common/masonry.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Author: AliceLanniste
+ * @Date: 2024-07-09
+
+ * @Description: 瀑布流
+-->
+
+<template>
+  <div></div>
+</template>
+
+<script name="masonry" setup></script>
+<style scoped lang="less"></style>
