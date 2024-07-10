c41a7541b5ca46db63a7b40bed84720361d6eab3
Alice Peng <1399789151@qq.com>
2024-07-10 11:15:04 +0800

    feat(component): 瀑布流增加样式

--- src/components/common/masonry.vue
+++ src/components/common/masonry.vue
@@ -6,8 +6,33 @@
 -->
 
 <template>
-  <div></div>
+  <div class="masonry-container">
+    <div class="masonry-list">
+      <div class="masonry-item"></div>
+    </div>
+  </div>
 </template>
 
 <script name="masonry" setup></script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.masonry {
+  &-container {
+    width: 100%;
+    height: 100%;
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
+
+  &-list {
+    position: relative;
+    width: 100%;
+  }
+
+  &-item {
+    position: absolute;
+    top: 0;
+    left: 0;
+    box-sizing: border-box;
+  }
+}
+</style>
