b37c86c14a36bc10cdafa0753ed12c8e109b9abd
Alice Peng <1399789151@qq.com>
2024-07-19 18:34:16 +0800

    feat(plugin): 新增workspacePlugin 测试

---
+++ packages/core/__tests__/plugin/workspacePlugin.spec.ts
@@ -0,0 +1,7 @@
+import { beforeEach, expect, test, describe } from 'vitest';
+import { initPlugin } from '../utils/setup.ts';
+
+describe('workspace', async () => {
+  // beforeEach(() => {});
+  // test('workspace:init', () => {});
+});
