1ebc0f6e77b7cf2b261ff1692603fad80d373d15
GeorgeSmith215 <77789341+GeorgeSmith215@users.noreply.github.com>
2024-08-12 17:51:11 +0800

    fix(plugin): fix the DringPlugin.ts that the cursor style grab and grabbing is not so much available (#505)
    Co-authored-by: GeorgeSmith <georgesmith163@163.com>

--- packages/core/plugin/DringPlugin.ts
+++ packages/core/plugin/DringPlugin.ts
@@ -32,13 +32,13 @@ export class DringPlugin implements IPluginTempl {
 
   startDring() {
     this.dragMode = true;
-    this.canvas.defaultCursor = 'grab';
+    this.canvas.setCursor('grab');
     this.editor.emit('startDring');
     this.canvas.renderAll();
   }
   endDring() {
     this.dragMode = false;
-    this.canvas.defaultCursor = 'default';
+    this.canvas.setCursor('default');
     this.canvas.isDragging = false;
     this.editor.emit('endDring');
     this.canvas.renderAll();
@@ -50,7 +50,7 @@ export class DringPlugin implements IPluginTempl {
     this.canvas.on('mouse:down', function (this: ExtCanvas, opt) {
       const evt = opt.e;
       if (evt.altKey || This.dragMode) {
-        This.canvas.defaultCursor = 'grabbing';
+        This.canvas.setCursor('grabbing');
         This.canvas.discardActiveObject();
         This._setDring();
         this.selection = false;
@@ -62,9 +62,10 @@ export class DringPlugin implements IPluginTempl {
     });
 
     this.canvas.on('mouse:move', function (this: ExtCanvas, opt) {
+      This.dragMode && This.canvas.setCursor('grab');
       if (this.isDragging) {
         This.canvas.discardActiveObject();
-        This.canvas.defaultCursor = 'grabbing';
+        This.canvas.setCursor('grabbing');
         const { e } = opt;
         if (!this.viewportTransform) return;
         const vpt = this.viewportTransform;
@@ -86,14 +87,13 @@ export class DringPlugin implements IPluginTempl {
           obj.selectable = true;
         }
       });
-      This.canvas.defaultCursor = 'default';
+      This.dragMode && This.canvas.setCursor('grab');
       this.requestRenderAll();
     });
   }
 
   _setDring() {
     this.canvas.selection = false;
-    this.canvas.defaultCursor = 'grab';
     this.canvas.getObjects().forEach((obj) => {
       obj.selectable = false;
     });
