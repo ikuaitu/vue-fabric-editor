1dfa7dfd46fccdb374e014ff1e195cbd2fb735ca
胡正 <zheng@sharecreators.com>
2024-09-18 11:19:46 +0800

    fix: 侧边栏显示隐藏按钮，右边比左边高

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,3 +1,4 @@
+
 <!--
  * @Author: 秦少卫
  * @Date: 2024-05-17 15:30:21
@@ -53,7 +54,7 @@
           <lang></lang>
         </div>
       </Header>
-      <Content style="display: flex; height: calc(100vh - 64px)">
+      <Content style="display: flex; height: calc(100vh - 64px); position:relative">
         <!-- 左侧区域 -->
         <div v-if="state.show" :class="`left-bar ${state.toolsBarShow && 'show-tools-bar'}`">
           <!-- 左侧菜单 -->
